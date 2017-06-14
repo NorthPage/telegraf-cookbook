6c1e4827a5f40a1dade326a5372c5a7efd2318ff
Cody.Lewandowski <cody.lewandowski@target.com>
2017-06-14 16:07:58 -0500

    switching from toml-rb to toml gem

--- resources/config.rb
+++ resources/config.rb
@@ -26,10 +26,10 @@ property :path, String, default: node['telegraf']['config_file_path']
 default_action :create
 
 action :create do
-  chef_gem 'toml-rb' do
+  chef_gem 'toml' do
     source node['telegraf']['rubysource']
     clear_sources true
-    version '~> 0.3.0'
+    version '~> 0.1.2'
     compile_time true if respond_to?(:compile_time)
   end
 
@@ -43,7 +43,7 @@ action :create do
   end
 
   file path do
-    content TOML.dump(config)
+    content TOML::Generator.new(config).body
     user 'root'
     group 'telegraf'
     mode '0644'
